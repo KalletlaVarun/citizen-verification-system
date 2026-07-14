c75644f7ebb25a402bf7690fbcfce8e1bade6cdf
Varun Sagar <varun.ixb33@example.com>
2026-07-14 12:56:42 +0530

    Remove database password from source code

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import mysql.connector
 connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="root",
+    password="YOUR_PASSWORD",
     database="smart_verification_system"
 )
 
